31d8873139e7f81eb360b3bb1bd64863928652ed
Logician724 <Logician742@gmail.com>
2021-04-18 16:16:21 +0200

    feat: Add link controller

--- api/links.controller.js
+++ api/links.controller.js
@@ -1,20 +1,66 @@
-
-module.exports.shortenLink = (req, res) => {
-  return res.send({
-    'msg': 'shorten link under construction',
-    'data': {
-      from: 'link',
-      to: 'link'       
-    }
+const Link = require('./links.model');
+const getRandomString = () => Math.random().toString(32);
+const createLink = async url => {
+  const link = new Link({
+    from: url,
+    to: getRandomString(),
   });
+  try {
+    await link.save();
+  } catch (error) {
+    if (error.code === 11000) {
+      // eslint-disable-next-line no-unused-vars
+      await createLink(url);
+    } else {
+      return error;
+    }
+  }
 };
 
-module.exports.getLink = (req, res) => {
-  return res.send({
-    'msg': 'get link under construction',
-    'data': {
-      from: 'link',
-      to: 'link'
+module.exports.shortenLink = async (req, res) => {
+  try {
+    await createLink(req.body.link);
+    const newLink = await Link.findOne({ from: req.body.link }).orFail();
+    return res.status(201).send({
+      msg: 'Successfully created shortened link',
+      data: newLink,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      msg: 'An Unknown error occurred while creating link',
+      err: error,
+    });
+  }
+};
+
+module.exports.getLink = async (req, res) => {
+  if (
+    req.params.link &&
+    typeof req.params.link === 'string' &&
+    req.params.link.length > 0
+  ) {
+    try {
+      const link = await Link.findOne({ to: req.params.link });
+      if (!link) {
+        return res.status(404).send({
+          msg: `Could not find a link for ${req.params.link}`,
+        });
+      }
+
+      return res.status(200).send({
+        msg: 'Found the link successfully',
+        data: link
+      });
+    } catch (error) {
+      return res.status(500).send({
+        msg: 'An unknown error occurred while retrieving link',
+        err: error,
+      });
     }
-  });
+  } else {
+    return res.status(422).send({
+      msg: `short link [${req.params.link}] is not valid`,
+      err: null,
+    });
+  }
 };
