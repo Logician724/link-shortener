cef1a9e181dad9aa603f715789eb7d0565d38406
Logician724 <Logician742@gmail.com>
2021-04-18 15:20:41 +0200

    chore: rename links router

--- api/links.router.js
+++ api/links.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const LinksController = require('./links.controller');
+const LinksValidator = require('./links.validator');
+
 router.get('/:link', LinksController.getLink);
-router.post('/', LinksController.shortenLink);
+router.post('/', LinksValidator.validateLink, LinksController.shortenLink);
 
 module.exports = router;
